335399c9844776d6c3c3433dbf973b22e4fc0210
pekarnya1 <97356877+pekarnya1@users.noreply.github.com>
2025-09-16 15:05:27 +0300

    Update task2.c

--- task2.c
+++ task2.c
@@ -1,4 +1,4 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
 int main()
@@ -7,8 +7,8 @@ int main()
 	int N = 12; // часы
 	int K = 21; // минуты
 	printf("Сейчас %d часов %d минут\n", N, K);
-	printf("Идет %d минута суток\n", K);
+	printf("Идет %d минута суток\n", K+N*60);
 	printf("До полуночи осталось %d часов и %d минут\n", 23 - N, 60 - K);
-	printf("С 8.00 прошло %d секунд\n", (N-8)*3600+K*60);
-	printf("Текущий час  = %d/24 суток  и текущая минута = %d/60 часа", N, K);
+	printf("С 8.00 прошло %d секунд\n", (N - 8) * 3600 + K * 60);
+	printf("Текущий час  = %.2f суток  и текущая минута = %.2f часа", (float)N/24, (float)K / 60);
 }
